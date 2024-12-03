1c220b79b7a5cbe2b50ae42d31172870b9788e04
hamzaaboulkhair <hamzaaboulkhair@gmail.com>
2024-12-03 18:30:27 +0100

    Ajout des fichiers du projet SimpleApp

---
+++ SimpleApp.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace SimpleApp.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // Ce code s'exécute avant chaque test (optionnel)
+        }
+
+        [Test]
+        public void TestAddition()
+        {
+            // Vérifie si 2 + 2 donne 4
+            Assert.That(2 + 2, Is.EqualTo(4), "L'addition est incorrecte");
+        }
+
+        [Test]
+        public void TestCustomMessage()
+        {
+            // Vérifie le contenu du message
+            string message = "Bienvenue dans SimpleApp";
+            Assert.That(message, Is.EqualTo("Bienvenue dans SimpleApp"), "Le message est incorrect");
+        }
+    }
+}
